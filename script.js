63fb4b0d847df0eab949e625af1bb0c3396c4cc4
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2022-07-12 23:41:57 -0300

    Started the operate()

--- script.js
+++ script.js
@@ -1,31 +1,56 @@
 const display = document.querySelector(".display");
-let currentNumber = 0;
+let currentNumber = 0,
+  previousNumber = 0;
+let reset = true;
 const numberButtons = document.querySelectorAll(".number");
+const clearButton = document.querySelector(".clear");
+const operatorsButtons = document.querySelectorAll(".operator");
 
-// Add numbers to the display
 numberButtons.forEach((button) =>
   button.addEventListener("click", populateDisplay)
 );
 
-const sum = (x, y) => x + y;
-const subtract = (x, y) => x - y;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
+clearButton.addEventListener("click", clearDisplay);
+
+operatorsButtons.forEach((button) => button.addEventListener("click", operate));
 
 function populateDisplay(e) {
+  if (reset) {
+    display.innerText = "";
+    reset = false;
+  }
   display.innerText += e.target.innerText;
   currentNumber = parseInt(display.innerText);
 }
 
-function operate(operator, x, y) {
-  switch (operator) {
-    case "sum":
-      return sum(x, y);
-    case "subtract":
-      return subtract(x, y);
-    case "multiply":
-      return multiply(x, y);
-    case "divide":
-      return divide(x, y);
-  }
+function clearDisplay() {
+  display.innerText = "";
+  currentNumber = 0;
+  previousNumber = 0;
 }
+
+function operate(e) {
+  let result = 0;
+  switch (e.target.classList[1]) {
+    case "sum":
+      result = previousNumber + currentNumber;
+      break;
+    case "subtract":
+      result = previousNumber - currentNumber;
+      break;
+    case "multiply":
+      result = previousNumber * currentNumber;
+      break;
+    case "divide":
+      result = previousNumber / currentNumber;
+      break;
+  }
+  display.innerText = result;
+  previousNumber = result;
+  reset = true;
+}
+
+// const sum = (x, y) => x + y;
+// const subtract = (x, y) => x - y;
+// const multiply = (x, y) => x * y;
+// const divide = (x, y) => x / y;
