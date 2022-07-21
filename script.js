a9ff841f2b2fb0ff342b0a22a306c60e3aa93745
Diego Moreira <diego.moreira529@gmail.com>
2022-07-21 10:18:46 -0300

    History working

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let currentNumber = 0,
 
 // Buttons
 const display = document.querySelector(".digits"),
+  historyDisplay = document.querySelector(".history"),
   currentOperator = document.querySelector(".current-operator"),
   numberButtons = document.querySelectorAll(".number"),
   pointButton = document.querySelector(".point"),
@@ -48,6 +49,7 @@ function insertPoint(e) {
 }
 
 function clearDisplay() {
+  historyDisplay.innerText = "";
   display.innerText = "0";
   currentNumber = 0;
   previousNumber = 0;
@@ -56,7 +58,7 @@ function clearDisplay() {
   currentOperator.innerText = "";
 }
 
-function makeExpression() {
+function operate() {
   switch (lastOperator) {
     case "sum":
       display.innerText = previousNumber + currentNumber;
@@ -78,63 +80,68 @@ function makeExpression() {
 }
 
 function sum() {
+  historyDisplay.innerText += ` ${currentNumber} +`;
   if (!firstOperation) {
-    makeExpression();
+    operate();
   } else {
     display.innerText = previousNumber + currentNumber;
     previousNumber += currentNumber;
   }
   currentNumber = 0;
-  currentOperator.innerText = "+";
+  // currentOperator.innerText = "+";
   firstOperation = false;
   lastOperator = "sum";
   pointButton.removeAttribute("disabled");
 }
 
 function subtract() {
+  historyDisplay.innerText += ` ${currentNumber} -`;
   if (!firstOperation) {
-    makeExpression();
+    operate();
   } else {
     previousNumber = currentNumber;
   }
   currentNumber = 0;
-  currentOperator.innerText = "-";
+  // currentOperator.innerText = "-";
   firstOperation = false;
   lastOperator = "subtract";
   pointButton.removeAttribute("disabled");
 }
 
 function multiply() {
+  historyDisplay.innerText += ` ${currentNumber} x`;
   if (!firstOperation) {
-    makeExpression();
+    operate();
   } else {
     previousNumber = currentNumber;
   }
   currentNumber = 0;
-  currentOperator.innerText = "x";
+  // currentOperator.innerText = "x";
   firstOperation = false;
   lastOperator = "multiply";
   pointButton.removeAttribute("disabled");
 }
 
 function divide() {
+  historyDisplay.innerText += ` ${currentNumber} ÷`;
   if (!firstOperation) {
-    makeExpression();
+    operate();
   } else {
     previousNumber = currentNumber;
   }
   currentNumber = 0;
-  currentOperator.innerText = "÷";
+  // currentOperator.innerText = "÷";
   firstOperation = false;
   lastOperator = "divide";
   pointButton.removeAttribute("disabled");
 }
 
 function equals() {
-  makeExpression();
+  historyDisplay.innerText += ` ${currentNumber} =`;
+  operate();
   currentNumber = Number(display.innerText);
   previousNumber = 0;
   firstOperation = true;
   lastOperator = "equals";
-  currentOperator.innerText = "=";
+  // currentOperator.innerText = "=";
 }
