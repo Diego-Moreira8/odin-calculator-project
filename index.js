959a8b18906fb84fb86539f0e63ac5a64932131c
Diego Moreira de Oliveira <diego.m.oliveira@una.br>
2023-11-22 21:13:55 -0300

    feat: handle insert float

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
 let currentNumber = 0;
+let isFloat = false;
 
 const numbersBtns = document.querySelectorAll(".number");
 const switchSignBtn = document.querySelector(".switch-sign");
+const pointBtn = document.querySelector(".point");
 
 window.onload = updateDisplay;
-window.onkeydown = handleNumberInput;
+window.onkeydown = (e) => {
+  if (!isNaN(parseInt(e.key))) handleNumberInput(e);
+  if (e.key === ".") handleInsertPoint();
+};
 numbersBtns.forEach((b) => b.addEventListener("click", handleNumberInput));
 switchSignBtn.addEventListener("click", handleSwitchSign);
+pointBtn.addEventListener("click", handleInsertPoint);
 
 function updateDisplay() {
   const display = document.querySelector(".display");
   display.textContent = currentNumber;
+
+  // If is float and has no digits before the floating point...
+  if (isFloat && `${currentNumber}`.indexOf(".") === -1) {
+    display.textContent = `${currentNumber}.0`;
+  }
 }
 
 function handleNumberInput(e) {
-  if (currentNumber.toString().length === 8) return;
+  if (
+    (isFloat && `${currentNumber}`.length === 9) ||
+    (!isFloat && `${currentNumber}`.length === 8)
+  ) {
+    return;
+  }
 
   let num;
 
   if (e.type === "click") {
-    num = parseInt(e.target.textContent);
+    num = e.target.textContent;
   } else if (e.type === "keydown") {
-    num = parseInt(e.key);
+    num = e.key;
   }
 
   if (!num) return;
 
-  currentNumber = parseInt(currentNumber.toString() + num.toString());
+  if (isFloat) {
+    let pointIndex = `${currentNumber}`.indexOf(".");
+    currentNumber =
+      pointIndex === -1
+        ? parseFloat(`${currentNumber}.${num}`)
+        : parseFloat(`${currentNumber}${num}`);
+  } else {
+    currentNumber = parseInt(`${currentNumber}${num}`);
+  }
+
   updateDisplay();
 }
 
 function handleSwitchSign() {
   currentNumber *= -1;
+  updateDisplay();
+}
+
+function handleInsertPoint() {
+  if (isFloat) return;
+  pointBtn.disabled = true;
+  isFloat = true;
   updateDisplay();
 }
